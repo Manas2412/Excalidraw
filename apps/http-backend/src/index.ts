d73c8cc307a0b753a625216105f5beae713f7985
Manas2412 <98726602+Manas2412@users.noreply.github.com>
2025-12-10 16:10:22 +0530

    added zod

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,13 +1,20 @@
-import express from 'express';
+import express, { json } from 'express';
 import jwt from "jsonwebtoken"
 import Auth_middleware from "./Auth_middleware"
-import { JWT_SECRET } from './config';
+import { JWT_SECRET } from '@repo/backend-common/config';
+import { CreateroomSchema, CreateUserSchema, SigninSchema } from '@repo/common/types';
 
 const app = express();
 
 
 app.post('signup', async (req, res) => {
-    const { username, password } = req.body;
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return ;
+    }
     // Handle user signup logic here
 
     res.json({
@@ -16,22 +23,35 @@ app.post('signup', async (req, res) => {
 
 }),
 
-app.post('signin', async (req, res) => {
-    // Handle user login logic here
+    app.post('signin', async (req, res) => {
+        const data = SigninSchema.safeParse(req.body);
+        if (!data.success) {
+            res.json({
+                message: "Incorrect inputs"
+            })
+            return;
+        }
 
-    const userId = 1;
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET);
-
-    res.json({
-        token
+        // Handle user login logic here
+        const userId = 1;
+        const token = jwt.sign({
+            userId
+        }, JWT_SECRET);
+        res.json({
+            token
+        })
     })
-})
 
 
 app.post('room', Auth_middleware, async (req, res) => {
     //db call
+    const data = CreateroomSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return ;
+    }
 
     res.json({
         roomId: 123
