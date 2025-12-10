2f8e7e45085a136d6e87ca7feaf1fd8809ae667b
Manas2412 <98726602+Manas2412@users.noreply.github.com>
2025-12-10 15:29:05 +0530

    added ws and http backed initials

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,43 @@
 import express from 'express';
+import jwt from "jsonwebtoken"
+import Auth_middleware from "./Auth_middleware"
+import { JWT_SECRET } from './config';
 
 const app = express();
+
+
+app.post('signup', async (req, res) => {
+    const { username, password } = req.body;
+    // Handle user signup logic here
+
+    res.json({
+        uesrId: 123
+    })
+
+}),
+
+app.post('signin', async (req, res) => {
+    // Handle user login logic here
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        token
+    })
+})
+
+
+app.post('room', Auth_middleware, async (req, res) => {
+    //db call
+
+    res.json({
+        roomId: 123
+    })
+})
+
 
 app.listen(3003, () => {
     console.log('HTTP server listening on port 3003');
